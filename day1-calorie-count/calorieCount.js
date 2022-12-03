e890ecf386cc80b49fdd759665d154dee28552b5
armmac1 <maciulskasarminas@gmail.com>
2022-12-03 22:51:07 +0200

    update

--- day1-calorie-count/calorieCount.js
+++ day1-calorie-count/calorieCount.js
@@ -2,6 +2,7 @@ import { dataSet } from "./dataSet.js";
 
 export const getBiggestCalorieSum = () => Math.max(...dataSet
   .split('\n\n')
-  .map(dirtyCalories => dirtyCalories.split('\n'))
-  .map((cur) => cur.reduce((sumCalories, currentCalories) => sumCalories += parseInt(currentCalories, 10), 0))
+  .map((dirtyCalories) => dirtyCalories
+    .split('\n')
+    .reduce((sumCalories, currentCalories) => sumCalories += parseInt(currentCalories, 10), 0))
 );
